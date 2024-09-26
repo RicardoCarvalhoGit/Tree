a95419efbbcf07abedf1738f6634bba12c6f0ba5
Ricardo Carvalho <ricardo.ceo.mkt@gmail.com>
2024-09-26 20:25:23 -0300

    fix: corrigido o comportramento do botão ao dar erro de download

--- tree-code/src/components/layout/buttons/dowloadButton/DowloadButton.tsx
+++ tree-code/src/components/layout/buttons/dowloadButton/DowloadButton.tsx
@@ -1,6 +1,6 @@
 export default function DowloadButton() {
     const handleDownload = () => {
-        const fileUrl = '../../../../../public/assets/docs/edital.pdf';
+        const fileUrl = '/assets/docs/edital.pdf';
         const link = document.createElement('a');
         link.href = fileUrl;
         link.download = 'edital.pdf';
