48d8f80324f2f94262a89579eeaf90f352c8329c
Ricardo Carvalho <ricardo.ceo.mkt@gmail.com>
2024-10-30 09:08:04 -0300

    feat: configurada a resposividade para home da landing page

--- tree-code/src/components/pages/landing/LandingPageComponent.tsx
+++ tree-code/src/components/pages/landing/LandingPageComponent.tsx
@@ -7,9 +7,10 @@ export default function LandingPage () {
     return (
         <section className={styles.landingPage}>
 
+
             <div className={styles.cta}>
                 <h1>
-                    Faça Parte Da <span>Mudança</span>
+                    Faça Parte Da <span>Mudança!</span>
                 </h1>
                 <p>
                     Mostre seu compromisso com o planeta e inspire outros a seguir seu exemplo.
