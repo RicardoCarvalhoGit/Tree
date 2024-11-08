1966f8c90cc6e9e8334a37efe842fd2cff2b21f6
pauloXv1 <paulosilvano1707@gmail.com>
2024-11-08 00:14:15 -0300

    reforma incompleta na tela, só estou testando

---
+++ tree-code/src/components/pages/ranking/Company/Companies.tsx
@@ -0,0 +1,33 @@
+export const companies = [
+    {
+        position: 1,
+        name:"coca-cola",
+        descricao:"A The Coca-Cola Company é uma empresa de bebidas que fabrica, vende e comercializa bebidas",
+        logo: "https://licensingcon.com.br/wp-content/uploads/2019/08/CocaCola.png",
+    },
+    {
+        position: 2,
+        name:"cacau show",
+        descricao:" A Cacau Show é uma empresa brasileira de chocolates finos",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/b/b0/Logo_Cacau_Show.png",
+    },
+    {
+        position: 3,
+        name:"Disney",
+        descricao:"empresa de filmes infantis",
+        logo: "https://1000marcas.net/wp-content/uploads/2019/12/XVideos-logo.jpg",
+    },
+    {
+        position: 4,
+        name:"vale",
+        descricao:"empresa de mineração e desmatamento e queimadas",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg4fY2mZocVmf3Hw-nQQwqZcdXI4J_Y4ZBvg&s",
+    },
+    {
+        position: 5,
+        name:"Petrobrás",
+        descricao:"empresa de petroléo",
+        logo: "https://w7.pngwing.com/pngs/862/975/png-transparent-petrobras-hd-logo-thumbnail.png",
+    },
+    
+];
--- tree-code/src/components/pages/ranking/RankingComponents.tsx
+++ tree-code/src/components/pages/ranking/RankingComponents.tsx
@@ -1,89 +1,37 @@
 import React from "react";
-import style from "./RankingComponents.module.css";
+import style from "./RankingComponents.module.css"
+import { companies } from "./Company/Companies";
 
-function Ranking() {
+
+interface Company {
+    position: number;
+    logo: string;
+    name: string;
+    descricao: string;
+}
+
+const Card: React.FC = () => {
     return (
-        <div className={style.container}>
-            <div className={style.card}>
-                <div className={style.card_Title}>
-                    <h1>RANKING TREE</h1>
-                </div>
-                <div className={style.card_Body}>
-                    <ul className={style.list}>
-                        <li className={style.list_Item}>
-                            <div className={style.list_Item_header}>
-                                <img className={style.list_Icon} src="https://licensingcon.com.br/wp-content/uploads/2019/08/CocaCola.png" alt="Logo CocaCola" />
-                            </div>
-
-                            <div className={style.list_Item_Body}>
-                                <span className={style.list_Item_Titule}>
-                                    POSIÇÃO:            1°
-                                </span>
-
-                                <span className={style.list_Item_Titule}>
-                                    EMPRESA:  Coca-cola
-                                </span>
-                                <span className={style.list_Item_Subtitle}>
-                                    pontos: 60
-                                </span>
-                                <div className={style.list_Item_Progress_Bar}>
-                                    <span className={style.Progress} style={{ width: '60%' }}></span>
-                                </div>
-                            </div>
-                        </li>
-
-                        <li className={style.list_Item}>
-                            <div className={style.list_Item_header}>
-                                <img className={style.list_Icon} src="https://fariart.com.br/blog/wp-content/uploads/2023/04/logo-fanta-2017.jpg" alt="Logo Fanta" />
-                            </div>
-                            <div className={style.list_Item_Body}>
-                                <span className={style.list_Item_Titule}>
-                                    POSIÇÃO 2°
-                                </span>
-
-                                <span className={style.list_Item_Titule}>
-                                    EMPRESA: Fanta
-                                </span>
-                                <span className={style.list_Item_Subtitle}>
-                                    pontos: 40
-                                </span>
-                                <div className={style.list_Item_Progress_Bar}>
-                                    <span className={style.Progress} style={{ width: '40%' }}></span>
-                                </div>
-                            </div>
-                        </li>
-
-                        <li className={style.list_Item}>
-                            <div className={style.list_Item_header}>
-                                <img className={style.list_Icon} src="https://fariart.com.br/blog/wp-content/uploads/2023/03/pepsi-2014-logo-1.jpg" alt="Logo Pepsi" />
-                            </div>
-                            <div className={style.list_Item_Body}>
-                                <span className={style.list_Item_Titule}>
-                                    POSIÇÃO 3°
-                                </span>
-
-                                <span className={style.list_Item_Titule}>
-                                    EMPRESA: Pepsi
-                                </span>
-
-                                <span className={style.list_Item_Subtitle}>
-                                    pontos: 10
-                                </span>
-                                <div className={style.list_Item_Progress_Bar}>
-                                    <span className={style.Progress} style={{ width: '10%' }}></span>
-                                </div>
-                            </div>
-                        </li>
-
-                    </ul>
-                </div>
-                <div>
-                    <button className={style.mini_Card}>
-                        <a href="#">Saiba mais</a>
-                    </button>
+    <div className={style.container}>
+        <div className={style.card}>
+        <div className={style.card_search}>
+            <form >
+                <label htmlFor="search" >PESQUISA</label>
+                <input name="search" id="search"/>
+            </form>
+        </div>
+            {companies.map((company) => (
+            <div key={company.position} className={style.card_item}>
+                <div className={style.position}>{company.position}</div>
+                <img src={company.logo} alt={company.name} className={style.card_logo}/>
+                <div className={style.card_info}>
+                    <h2>{company.name}</h2>
+                    <p>{company.descricao}</p>
                 </div>
             </div>
+            ))}
         </div>
-    )
-}
-export default Ranking;
+    </div>
+    );
+};
+export default Card;
